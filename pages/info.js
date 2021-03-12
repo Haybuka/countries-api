1c561f9f0afe2c5dacb9d1bbbc61ffadb02330c8
haybuka <paschal.nduleo@gmail.com>
2021-03-12 15:00:10 +0100

    info page 50% structured,and made responsive

--- pages/info.js
+++ pages/info.js
@@ -1,6 +1,8 @@
 
 let dark = document.querySelector('.dark-mode')
 let main = document.querySelector('main')
+let loader = document.querySelector('.loader')
+let results = document.createElement('section')
 
 dark.addEventListener('click',(e)=>{
     let icon = dark.querySelector('i');
@@ -26,7 +28,7 @@ function createElements(element,value){
        let p = document.createElement('p');
        spanFirst.innerHTML = element;
        spanSecond.innerHTML = value;
-       p.append(spanFirst,' ',spanSecond);
+       p.append(spanFirst,' : ',spanSecond);
    return p;
 
 }
@@ -35,69 +37,83 @@ function createElements(element,value){
 let input = localStorage.getItem('country');
 
 function parseCountry ({flag,name,population,region,capital,nativeName,subregion,topLevelDomain,currencies,languages,borders}){
- 
+ console.log(`${flag}, ${name}, ${population},${region},${capital},${nativeName},${subregion},${topLevelDomain},${currencies},${languages},${borders}`)
     let money = '';
     let language = '';
     let border = ''
     let domains = ''
-    topLevelDomain.forEach((domain)=>{
-        domains +=  ' ' + domain + ' '
-     })
      currencies.forEach((currency)=>{
          money +=  ' ' + currency.name + ' '
      })
      languages.forEach((speaks)=>{
-        language += ' ' + speaks.name + ' '
+        language += ' ' + speaks.name + ' ,'
     })
-    borders.forEach((bound)=>{
-        border += ' ' + bound + ' '
+    // borders.forEach((bound)=>{
+    //     border += ' ' + bound + ' '
+    // })
 
-    })
-console.log(`native name : ${nativeName} ,domain : ${domains} , currency : ${money}, language : ${language}, border : ${border}, subregion : ${subregion}`)
+      let mainDiv = document.createElement('div')
+       //flag
+       let img = document.createElement('img')
+       img.src = flag;
+       let imgDiv = document.createElement('div')
+       imgDiv.setAttribute('class',"img-div")
+       imgDiv.append(img)
+       //name
+       let header = document.createElement('h3')
+         if(name.length > 20){
+            // let names = 'United Kingdom of Great Britain and Northern Ireland'
+            let newName = [...name.split(' ')]
+            let adjustedName =  newName.splice(0,3)
+            name = adjustedName.toString().split(',').join(' ') + ' ...'
+         }
+       header.innerHTML = name;
+       //    native name
+       let natName = createElements('Native Name',nativeName);
+   
+        // subRegion
+       let subReg = createElements('Sub Region',subregion);
+     
+       //population
+       let popParent = createElements('population',population);
+       // money
+       let moneyShow = createElements('currencies',money);
+         // language
+       let lingua = createElements('languages',language);
+        
+        //  region
+       let regionParent = createElements('region',region);
+        //    top level
+       let topLevel = createElements('Top Level Domain',topLevelDomain);
       
-    //   let mainDiv = document.createElement('div')
-    //    //flag
-    //    let img = document.createElement('img')
-    //    img.src = flag;
-    //    let imgDiv = document.createElement('div')
-    //    imgDiv.setAttribute('class',"img-div")
-    //    imgDiv.append(img)
-    //    //name
-    //    let header = document.createElement('h3')
-    //      if(name.length > 20){
-    //         // let names = 'United Kingdom of Great Britain and Northern Ireland'
-    //         let newName = [...name.split(' ')]
-    //         let adjustedName =  newName.splice(0,3)
-    //         name = adjustedName.toString().split(',').join(' ') + ' ...'
-    //      }
-    //    header.innerHTML = name;
-    //    //population
-    //    let popParent = createElements('population',population);
-    //    //region
-    //    let regionParent = createElements('region',region);
-    //    //capital
-    //    let capitalParent = createElements('capital',capital)
-    // //   console.log(header.innerText, capitalParent.innerText)
-    //   let valueDiv = document.createElement('div')
-    //   valueDiv.append(header,popParent,regionParent,capitalParent)
-    //   valueDiv.setAttribute('class','value-div')
-    //   mainDiv.append(imgDiv,valueDiv);
-    //   mainDiv.setAttribute('class','results-div')
-    //   results.append(mainDiv)
-    //   main.append(results)
+     //  capital
+       let capitalParent = createElements('capital',capital)
+    //   console.log(header.innerText, capitalParent.innerText)
+      let valueDiv = document.createElement('div')
+      let rightDiv = document.createElement('div')
+      valueDiv.append(header,natName,popParent,regionParent,subReg,capitalParent)
+      rightDiv.append(topLevel,moneyShow,lingua)
+      valueDiv.setAttribute('class','value-div')
+      mainDiv.append(imgDiv,valueDiv,rightDiv);
+      mainDiv.setAttribute('class','results-div contain')
+      results.append(mainDiv)
+      main.append(results)
 }
 
 let searchCountry = async (input) =>{
 try {
     let result = await axios.get(`https://restcountries.eu/rest/v2/name/${input}`)
     //display none the preloader
+    loader.classList.add('display')
     let data = result.data;
     data.forEach(country => {
     parseCountry(country)  
-    // console.log(country)
+    // console.log(country.name)
+    // console.log(country.topLevelDomain)
  });
 } catch (e) {
     //display none the preloader
+    loader.classList.add('display')
     country = {
         flag:'/image/error.jpg',
         name:input,
@@ -105,7 +121,7 @@ try {
         region:'N/A',
         capital:'N/A'
     }
-    // parseCountry(country)
+    parseCountry(country)
  }
 }
 searchCountry(input)
