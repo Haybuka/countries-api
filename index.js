8b6b156dc53f31a8f3da3786d036a0d92970d3fb
haybuka <paschal.nduleo@gmail.com>
2021-03-12 18:50:23 +0100

    added preloader to the index page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let main = document.querySelector('main')
 let form = document.querySelector('form')
 let dark = document.querySelector('.dark-mode')
 let selectRegion = document.querySelector('#filterSelect')
+let loader = document.querySelector('.loader')
 let results = document.createElement('section')
 results.remove()
 //search by region
@@ -43,6 +44,8 @@ dark.addEventListener('click',(e)=>{
 
 results.setAttribute('class'," results contain")
 
+//function to create elements
+
 function createElements(element,value){
        let spanFirst = document.createElement('span');
        let spanSecond = document.createElement('span');
@@ -54,59 +57,69 @@ function createElements(element,value){
 
 }
 
-function parseCountry ({flag,name,population,region,capital,nativeName,subregion,topLevelDomain,currencies,languages,borders}){
-    // console.log(`nativeName : ${nativeName}, subregion : ${subregion}`)
+//Parse Countries and destructure api data
 
-   
-// console.log(`native name : ${nativeName} ,domain : ${domains} , currency : ${money}, language : ${language}, border : ${border}, subregion : ${subregion}`)
-      
+function parseCountry ({flag,name,population,region,capital,nativeName,subregion,topLevelDomain,currencies,languages,borders}){
+  
       let mainDiv = document.createElement('div')
-       //flag
+
+       //flag and flag div handling
        let img = document.createElement('img')
        img.src = flag;
        let imgDiv = document.createElement('div')
        imgDiv.setAttribute('class',"img-div")
        imgDiv.append(img)
+
        //name
        let header = document.createElement('h3')
+
+       //shortens country name to fit box
          if(name.length > 20){
-            // let names = 'United Kingdom of Great Britain and Northern Ireland'
             let newName = [...name.split(' ')]
             let adjustedName =  newName.splice(0,3)
             name = adjustedName.toString().split(',').join(' ') + ' ...'
          }
        header.innerHTML = name;
-       //population and number formatter
-       let formatter = new Intl.NumberFormat('en-US', {
 
-    });
-    
-    let populations = formatter.format(population); 
+       //population and number formatter
+       let formatter = new Intl.NumberFormat('en-US');
+       let populations = formatter.format(population); 
            let popParent = createElements('population',populations);
-       //region
+
+       //region 
        let regionParent = createElements('region',region);
+
        //capital
        let capitalParent = createElements('capital',capital)
-    //   console.log(header.innerText, capitalParent.innerText)
-      let valueDiv = document.createElement('div')
-      valueDiv.append(header,popParent,regionParent,capitalParent)
-      valueDiv.setAttribute('class','value-div')
-      mainDiv.append(imgDiv,valueDiv);
-      mainDiv.setAttribute('class','results-div')
-      results.append(mainDiv)
-      main.append(results)
+
+       //Handling image display and appending
+       let valueDiv = document.createElement('div')
+       valueDiv.append(header,popParent,regionParent,capitalParent)
+       valueDiv.setAttribute('class','value-div')
+       mainDiv.append(imgDiv,valueDiv);
+       mainDiv.setAttribute('class','results-div')
+       results.append(mainDiv)
+       main.append(results)
 }
+
+//Handle search and call search
+
 let searchCountry = async (input) =>{
+    //try catch block for async usage
+
 try {
     let result = await axios.get(`https://restcountries.eu/rest/v2/name/${input}`)
     //display none the preloader
+    loader.classList.add('display')
+
     let data = result.data;
     data.forEach(country => {
     parseCountry(country)     
-    // console.log(country)
  });
 } catch (e) {
     //display none the preloader
+    loader.classList.add('display')
+
     country = {
         flag:'/image/error.jpg',
         name:input,
@@ -118,6 +131,7 @@ try {
  }
 }
 
+//Display result on input used
 
 form.addEventListener('submit',async function (e) {
     e.preventDefault();
@@ -137,9 +151,13 @@ form.addEventListener('submit',async function (e) {
      })
    })
 })
-//default search on load
+
+
+//Display results on load 
+
 async function defaultLoad (){
 let result = await axios.get('https://restcountries.eu/rest/v2/all')
+loader.classList.add('display')
 result.data.forEach(async function(data){
     await parseCountry(data)
     //adapting the windows open on page load
